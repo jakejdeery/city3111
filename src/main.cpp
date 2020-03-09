861e78ee7d0ed0285cae6c53a5e7e9248b5f6605
Were it so easy <19471697+axessta@users.noreply.github.com>
2020-03-09 00:39:22 +0000

    Huge progress with MPI implementation. OO it next?

--- src/main.cpp
+++ src/main.cpp
@@ -14,15 +14,12 @@ double getFit(double x, double y) {
 
 int main(int argc, char** argv) {
 	// vars
-	array<double, 3> bestFit;
-	double startBound = -16384.0;
-	double endBound = 16384.0;
+	double startBound = -128.0;
+	double endBound = 128.0;
+	double precision = 0.01;
 	double boundRange = abs(startBound - endBound);
-	double fitness;
-	int worldSize;
-	int worldRank;
-	int i;
-	uTimeOut deltaTime;
+	int worldSize = 0;
+	int worldRank = 0;
 
 	// setup
 	locale systemLocale("en_GB.UTF-8");
@@ -42,44 +39,84 @@ int main(int argc, char** argv) {
 
 	// main system echo
 	if(worldRank == 0) {
+		// vars
+		int iTotal = 0;
+		double bestFit[4];
+		uTimeOut deltaTime;
+		
 		cout << "[I] Op1 program by Jake Deery, 2020" << "\n";
 		cout << "[I] Program commencing, please wait . . . " << "\n";
 		cout << "\n";
-	}
-
-	// vars
-	startBound = startBound + (bunchSize * worldRank);
-	endBound = endBound - (bunchSize  * ((worldSize - 1) - worldRank));
-	i = 0;
-
-	// getting time
-	auto startTime = uTimeGet::now();
-
-	// main for loop
-	for(double x = startBound; x < endBound + .1; x += .1) {
-		for(double y = startBound; y < endBound + .1; y += .1) {
-			fitness = getFit(x,y);
-
-			if(fitness < bestFit[2]) {
-				bestFit[0] = x;
-				bestFit[1] = y;
-				bestFit[2] = fitness;
-			};
-
-			// alert counter
-			i++;
+		
+		// getting time
+		auto startTime = uTimeGet::now();
+		
+		// for loop for slave messages
+		for(int i = 0; i < (worldSize - 1); i++) {
+			// vars
+			int iCurrent = 0;
+			double currentFit[4];
+			
+			// wait for news
+			MPI_Recv(&currentFit[0], 1, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&currentFit[1], 1, MPI_DOUBLE, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&currentFit[2], 1, MPI_DOUBLE, MPI_ANY_SOURCE, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&currentFit[3], 1, MPI_DOUBLE, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&iCurrent, 1, MPI_INT, MPI_ANY_SOURCE, 4, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			
+			// calculate iterations
+			iTotal += iCurrent;
+			
+			// if the received values are the best, keep them
+			if(currentFit[2] < bestFit[2]) {
+				bestFit[0] = currentFit[0];
+				bestFit[1] = currentFit[1];
+				bestFit[2] = currentFit[2];
+			}
 		}
+		
+		// end and calculate time
+		auto endTime = uTimeGet::now();
+		deltaTime = endTime - startTime;
+		
+		cout << "[I] The program ran through " << iTotal << " loops in " << deltaTime.count() / 1000 << " ms" << "\n";
+		cout << "[I] The best fit was " << bestFit[2] << " at {" << bestFit[0] << "," << bestFit[1] << "}" << "\n";
+		cout << "[E] Program quits" << "\n";
+		cout << "\n";
+	} else {
+		// vars
+		double localFit[3];
+		double fitness = 0.0;
+		int i = 0;
+		
+		// defines
+		startBound = startBound + (bunchSize * worldRank);
+		endBound = endBound - (bunchSize  * ((worldSize - 1) - worldRank));
+
+		// main for loop
+		for(double x = startBound; x < endBound + precision; x += precision) {
+			for(double y = startBound; y < endBound + precision; y += precision) {
+				fitness = getFit(x,y);
+
+				if(fitness < localFit[2]) {
+					localFit[0] = x;
+					localFit[1] = y;
+					localFit[2] = fitness;
+				};
+
+				// alert counter
+				i++;
+			}
+		}
+		
+		// send the results to the master process
+		MPI_Send(&localFit[0], 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+		MPI_Send(&localFit[1], 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
+		MPI_Send(&localFit[2], 1, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
+		MPI_Send(&worldRank, 1, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
+		MPI_Send(&i, 1, MPI_INT, 0, 4, MPI_COMM_WORLD);
 	}
-
-	// end and calculate time
-	auto endTime = uTimeGet::now();
-	deltaTime = endTime - startTime;
-
-	// report findings
-	cout << "[I] Process " << worldRank << " ran through " << i << " loops in " << deltaTime.count() / 1000 << "ms\n";
-	cout << "[I] Process " << worldRank << " says best fit was " << bestFit[2] << " found at [" << bestFit[0] << "," << bestFit[1] << "]\n";
-	cout << "\n";
-
+	
 	// end process
 	MPI_Finalize();
 	return 0;
