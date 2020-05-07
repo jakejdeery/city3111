b7c47ea51095c19846ff8a645ea787d9b157c543
Jake Deery <jacobdeery@msn.com>
2020-05-07 22:37:54 +0100

    Fixed another issue in my code -- now stripe-searching rather than cube searching. Thanks Andy!

--- src/opClass.cpp
+++ src/opClass.cpp
@@ -54,30 +54,40 @@ float64_t opClass::doSearch(float64_t precision) {
 	const float64_t cWorldSize = worldSize - 1.0;
 	
 	// vars
-	float64_t startBound = 0;
-	float64_t endBound = 0;
+	float64_t startXBound = 0;
+	float64_t endXBound = 0;
+	float64_t startYBound = 0;
+	float64_t endYBound = 0;
 	float64_t bestFit[3];
 	float64_t bestRank = 0.0;
 	float64_t effort = 0.0;
+	float64_t totalEffort = 0.0;
 	float64_t bunchSize = 0.0;
 	uTimeOut deltaTime;
-
+	
 	// defines
 	bunchSize = defaultBoundRange / worldSize;
 	
 	// begin calculating current bunches -- basic
-	startBound = defaultStartBound + (bunchSize * worldRank);
-	endBound = startBound + bunchSize;
+	startXBound = defaultStartBound + (bunchSize * worldRank);
+	endXBound = startXBound + bunchSize; // note: corrected for precision to avoid overlap
 	
 	// calculating current bunches -- fix for precision to avoid overlap
-	if(endBound < 0) endBound += precision;
-	else if(endBound == 0 || endBound > 0) endBound -= precision;
+	if(endXBound < 0) endXBound += precision;
+	else if(endXBound == 0 || endXBound > 0) endXBound -= precision;
 	
 	// calculating current bunches -- re-adjust last process to cover entire range
-	if(worldRank == cWorldSize) endBound += precision;
+	if(worldRank == cWorldSize) endXBound += precision;
+	
+	// set y-bounds to cover entire range
+	startYBound = -512.0;
+	endYBound = 512.0;
 
 	// declaare start of search
 	if(worldRank == 0) cout << "\n[I] Starting op search . . . " << flush;
+	
+	cout << worldRank << " " << startXBound << "\n";
+	cout << worldRank << " " << endXBound << "\n" << "\n";
 
 	//
 	// main for loop
@@ -85,8 +95,8 @@ float64_t opClass::doSearch(float64_t precision) {
 	// getting time
 	auto startTime = uTimeGet::now();
 
-	for(float64_t x = startBound; x < endBound + precision; x += precision) {
-		for(float64_t y = startBound; y < endBound + precision; y += precision) {
+	for(float64_t x = startXBound; x < endXBound + precision; x += precision) { // x = limited
+		for(float64_t y = startYBound; y < endYBound + precision; y += precision) { // y = do whole search-space
 			float64_t fitness = getFit(x,y);
 
 			if(fitness < bestFit[2]) {
@@ -104,6 +114,9 @@ float64_t opClass::doSearch(float64_t precision) {
 	auto endTime = uTimeGet::now();
 	deltaTime = endTime - startTime;
 	
+	// copy master effort to total
+	totalEffort += effort;
+	
 	// alert done
 	if(worldRank == 0) cout << "done" << "\n";
 
@@ -116,13 +129,15 @@ float64_t opClass::doSearch(float64_t precision) {
 		for(uint64_t i = 0; i < (worldSize - 1); i++) { // -1 because we have already stored data from proc#0
 			// vars
 			float64_t currentFit[3];
+			float64_t currentEffort = 0.0;
 			uint64_t currentRank = 0;
 
 			// wait for news
 			MPI_Recv(&currentFit[0], 1, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 			MPI_Recv(&currentFit[1], 1, MPI_DOUBLE, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 			MPI_Recv(&currentFit[2], 1, MPI_DOUBLE, MPI_ANY_SOURCE, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			MPI_Recv(&currentRank, 1, MPI_INT, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&currentEffort, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&currentRank, 1, MPI_INT, MPI_ANY_SOURCE, 4, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
 			// if the received values are the best, keep them
 			if(currentFit[2] < bestFit[2]) {
@@ -131,10 +146,13 @@ float64_t opClass::doSearch(float64_t precision) {
 				bestFit[2] = currentFit[2];
 				bestRank = currentRank;
 			}
+			
+			// update totalEffort
+			totalEffort += currentEffort;
 		}
 		
 		cout << "[I] Each process ran through approx. " << effort << " loops each" << "\n";
-		cout << "[I] The program ran through approx. " << effort * worldSize << " loops in total" << "\n";
+		cout << "[I] The program ran through approx. " << totalEffort << " loops in total" << "\n";
 		cout << "[I] This took approx. " << deltaTime.count() << " ms per process" << "\n";
 		cout << "[I] The best fit was " << bestFit[2] << " at {" << bestFit[0] << ";" << bestFit[1] << "}" << "\n";
 		cout << "[I] The best fit was found by process " << bestRank + 1 << " of " << worldSize << "\n";
@@ -144,7 +162,8 @@ float64_t opClass::doSearch(float64_t precision) {
 		MPI_Send(&bestFit[0], 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
 		MPI_Send(&bestFit[1], 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
 		MPI_Send(&bestFit[2], 1, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
-		MPI_Send(&worldRank, 1, MPI_INT, 0, 3, MPI_COMM_WORLD);
+		MPI_Send(&effort, 1, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
+		MPI_Send(&worldRank, 1, MPI_INT, 0, 4, MPI_COMM_WORLD);
 	}
 
 	// end process
